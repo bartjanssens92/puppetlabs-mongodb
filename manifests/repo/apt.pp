a96b04db86ea5a50589272d50b699ef7c8427ea8
bjanssens <bjanssens@inuits.eu>
2015-10-20 13:23:55 +0200

    Bugfix: missing comma
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -13,7 +13,7 @@ class mongodb::repo::apt inherits mongodb::repo {
       include  => {
         'src'  => false,
         'deb'  => true,
-      }
+      },
       key        => {
         'id'     => '492EAFE8CD016A07919F1D2B9ECBEC467F0CEB10',
         'server' => 'hkp://keyserver.ubuntu.com:80',
